38bb13307c378a78f32f99f452ea70bfa851fa5f
yeeming <yeeming0771@foxmail.com>
2020-02-22 18:02:45 +0800

    Update lib.rs(unfinished: -s)

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::error::Error;
 use std::fs;
 
+static mut FLAG: bool = false;
+
 pub struct Config {
     pub command: String,
     pub filename: String,
@@ -70,10 +72,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub fn show(cmd: &str, contents: &str) -> Vec<String> {
     match cmd {
         // Show all
-        "-A" | "--show-all" | "-ET" | "-TE" => contents
+        "-A" | "--show-all" | "-vET" | "-vTE" | "-EvT" | "-TvE" | "-ETv" | "-TEv" => contents
             .lines()
             .map(|line| {
-                let tmp = line.replace("\t", "^I");
+                let mut collecter = Vec::<String>::new();
+
+                for mut ch in line.chars() {
+                    if !ch.is_ascii() {
+                        ch = '@';
+                    }
+
+                    collecter.push(ch.to_string());
+                }
+
+                let mut tmp = String::new();
+
+                for st in collecter.iter() {
+                    tmp = format!("{}{}", tmp, st);
+                }
+
+                tmp = tmp.replace("\t", "^I");
+
                 format!("{}$", tmp)
             })
             .collect(),
@@ -93,7 +112,8 @@ pub fn show(cmd: &str, contents: &str) -> Vec<String> {
                         }
                     }
 
-                    if index == true {
+                    if index {
+                        // nonempty
                         count += 1;
                         format!("{:>6}  {}", count, line)
                     } else {
@@ -102,6 +122,29 @@ pub fn show(cmd: &str, contents: &str) -> Vec<String> {
                 })
                 .collect()
         }
+        // -vE
+        "-e" | "-vE" | "-Ev" => contents
+            .lines()
+            .map(|line| {
+                let mut collecter = Vec::<String>::new();
+
+                for mut ch in line.chars() {
+                    if !ch.is_ascii() {
+                        ch = '@';
+                    }
+
+                    collecter.push(ch.to_string());
+                }
+
+                let mut tmp = String::new();
+
+                for st in collecter.iter() {
+                    tmp = format!("{}{}", tmp, st);
+                }
+
+                format!("{}$", tmp)
+            })
+            .collect(),
         // Display $ at end of each line
         "-E" | "--show-ends" => contents.lines().map(|line| format!("{}$", line)).collect(),
         // Number all output lines
@@ -116,6 +159,62 @@ pub fn show(cmd: &str, contents: &str) -> Vec<String> {
                 })
                 .collect()
         }
+        // Suppress repeated empty output lines
+        "-s" | "--squeeze-blank" => {
+            contents
+                .lines()
+                .map(|line| {
+                    let mut index = false;
+
+                    for ch in line.bytes() {
+                        if ch != b' ' | b'\t' {
+                            index = true;
+                            break;
+                        }
+                    }
+
+                    unsafe {
+                        if index {
+                            // nonempty
+                            FLAG = false;
+                            line.to_string()
+                        } else {
+                            if FLAG {
+                                String::from("\u{8}") // unsupplied in Rust
+                            } else {
+                                FLAG = true;
+                                String::from(" ")
+                            }
+                        }
+                    }
+                })
+                .collect()
+        }
+        // -vT
+        "-t" | "-vT" | "-Tv" => contents
+            .lines()
+            .map(|line| {
+                let mut collecter = Vec::<String>::new();
+
+                for mut ch in line.chars() {
+                    if !ch.is_ascii() {
+                        ch = '@';
+                    }
+
+                    collecter.push(ch.to_string());
+                }
+
+                let mut tmp = String::new();
+
+                for st in collecter.iter() {
+                    tmp = format!("{}{}", tmp, st);
+                }
+
+                tmp = tmp.replace("\t", "^I");
+
+                tmp
+            })
+            .collect(),
         // Display TAB characters as ^I
         "-T" | "--show-tabs" => contents
             .lines()
@@ -126,13 +225,36 @@ pub fn show(cmd: &str, contents: &str) -> Vec<String> {
             .collect(),
         // Ignore
         "-u" => contents.lines().map(|line| line.to_string()).collect(),
+        // Use ^M notation, except for LFD and TAB
+        "-v" | "--show-nonascii" => contents
+            .lines()
+            .map(|line| {
+                let mut collecter = Vec::<String>::new();
+
+                for mut ch in line.chars() {
+                    if !ch.is_ascii() {
+                        ch = '@';
+                    }
+
+                    collecter.push(ch.to_string());
+                }
+
+                let mut tmp = String::new();
+
+                for st in collecter.iter() {
+                    tmp = format!("{}{}", tmp, st);
+                }
+
+                tmp
+            })
+            .collect(),
         // Display this help and exit
         "--help" => vec![String::from(
             "\
 name:    minicat
-version: 0.2.0
+version: 1.0.0
 author:  Wang-yeeming <yeeming0771@foxmail.com>
-about:   A self-made CLI tool, simply implement of the cat.
+about:   A self-made CLI tool, simple implement of the cat.
 
 USAGE
 
@@ -140,19 +262,23 @@ USAGE
 
 OPTIONS
 
-	-A, --show-all           equivalent to -ET
+	-A, --show-all           equivalent to -vET
 	-b, --number-nonblank    number nonempty output lines, overrides -n
+	-e                       equivalent to -vE
 	-E, --show-ends          display $ at end of each line
 	-n, --number             number all output lines
+	-s, --squeeze-blank      suppress repeated empty output lines
+	-t                       equivalent to -vT
 	-T, --show-tabs          display TAB characters as ^I
 	-u                       (ignore)
+	-v, --show-nonascii      display non-ASCII characters as @
 	    --help               display this help and exit
 	    --version            output version information and exit
-	
+
 Have a try!",
         )],
         // Output version information and exit
-        "--version" => vec!["minicat 0.2.0".to_string()],
+        "--version" => vec!["minicat 1.0.0".to_string()],
         // Unidentified command
         _ => vec![format!(
             "Cannot found command named: {}.
@@ -287,19 +413,36 @@ If you wanna output directly, try: minicat -u [FILE]"
 
     #[test]
     fn use_show_all_cmd() {
-        let text = generate_text();
+        let text = String::from("\t\t我中国人、\n日本语了解不能。");
 
-        assert_eq!(
-            show("--show-all", &text),
-            vec![
-                "#include <iostream>$",
-                "$",
-                "int main() {$",
-                "^Istd::cout << \"Hello, world!\" << std::endl;$",
-                "$",
-                "^Ireturn 0;$",
-                "}$"
-            ]
-        )
+        assert_eq!(show("--show-all", &text), vec!["^I^I@@@@@$", "@@@@@@@@$"])
+    }
+
+    #[test]
+    fn use_show_nonascii_cmd() {
+        let text = String::from("你好世界\nHello world");
+
+        assert_eq!(show("-v", &text), vec!["@@@@", "Hello world"])
+    }
+
+    #[test]
+    fn use_t_cmd() {
+        let text = String::from("你好\t世界\n\t\tHello world");
+
+        assert_eq!(show("-t", &text), vec!["@@^I@@", "^I^IHello world"])
+    }
+
+    #[test]
+    fn use_e_cmd() {
+        let text = String::from("你好\n\t世界");
+
+        assert_eq!(show("-e", &text), vec!["@@$", "\t@@$"])
+    }
+
+    #[test]
+    fn use_squeeze_blank_cmd() {
+        let text = String::from("\n\n\n\n\n");
+
+        assert_eq!(show("-s", &text), vec!["\n"])
     }
 }
